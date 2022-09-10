498ab30e5886c0364565bb499acd994345cff987
Ana2777 <110324737+Ana2777@users.noreply.github.com>
2022-09-10 13:45:55 +0500

    Delete Launching.java

--- src/Launching.java
+++
@@ -1,9 +0,0 @@
-public class Launching {
-    public static void work() {
-        Book oneBook = new Book("Название 1", "Автор 1", 1866);
-        Author book1 = new Author("Фамилия 1","Имя 1");
-        oneBook.setYearOfPublication(2018);
-        System.out.print(book1);
-        System.out.print(oneBook);
-    }
-}
